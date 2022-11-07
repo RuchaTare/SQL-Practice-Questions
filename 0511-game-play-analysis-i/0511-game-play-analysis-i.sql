524722c6c5c9f2c75c763470659cf70b87a506e0
Rucha Shashikant Tare <76004350+RuchaTare@users.noreply.github.com>
2022-11-07 14:39:56 +0000

    Time: 691 ms (80.63%), Space: 0B (100.00%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,38 @@
+# Write your MySQL query statement below
+
+
+select player_id , min(event_date) as first_login 
+     from Activity 
+    group by player_id
+
+# SELECT
+#   X.player_id,
+#   X.event_date AS first_login
+# FROM
+#   (
+#     SELECT
+#       A.player_id,
+#       A.event_date,
+#       RANK() OVER (
+#         PARTITION BY
+#           A.player_id
+#         ORDER BY
+#           A.event_date
+#       ) AS rnk
+#     FROM
+#       Activity A
+#   ) X
+# WHERE
+#   X.rnk = 1;
+
+# SELECT DISTINCT
+#   A.player_id,
+#   FIRST_VALUE(A.event_date) OVER (
+#     PARTITION BY
+#       A.player_id
+#     ORDER BY
+#       A.event_date
+#   ) AS first_login
+# FROM
+#   Activity A;
+

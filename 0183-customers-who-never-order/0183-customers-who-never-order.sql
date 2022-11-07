53e8cd99ad28ab73704d1478cb64dcbb5ae3a8b3
Rucha Shashikant Tare <76004350+RuchaTare@users.noreply.github.com>
2022-11-07 13:13:53 +0000

    Time: 819 ms (27.38%), Space: 0B (100.00%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+#what do we need:
+# We need customer name from customers table 
+# by matching id from customer table to id customerid in orders to find people who
+#have no orders
+
+#approach 
+#un match customer - id to orders - customerid
+
+SELECT  name as Customers from Customers c
+WHERE c.id not in (select customerId from orders)

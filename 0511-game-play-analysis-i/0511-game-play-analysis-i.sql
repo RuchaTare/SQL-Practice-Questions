076080793c5a4efc8efb386cbf4cea0f63acd4a1
Rucha Shashikant Tare <76004350+RuchaTare@users.noreply.github.com>
2022-11-07 14:40:14 +0000

    Time: 845 ms (38.48%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
 
 
-select player_id , min(event_date) as first_login 
-     from Activity 
-    group by player_id
+# select player_id , min(event_date) as first_login 
+#      from Activity 
+#     group by player_id
 
 # SELECT
 #   X.player_id,
@@ -25,14 +25,14 @@ select player_id , min(event_date) as first_login
 # WHERE
 #   X.rnk = 1;
 
-# SELECT DISTINCT
-#   A.player_id,
-#   FIRST_VALUE(A.event_date) OVER (
-#     PARTITION BY
-#       A.player_id
-#     ORDER BY
-#       A.event_date
-#   ) AS first_login
-# FROM
-#   Activity A;
+SELECT DISTINCT
+  A.player_id,
+  FIRST_VALUE(A.event_date) OVER (
+    PARTITION BY
+      A.player_id
+    ORDER BY
+      A.event_date
+  ) AS first_login
+FROM
+  Activity A;
 

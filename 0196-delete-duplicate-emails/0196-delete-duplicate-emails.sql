6f35858770059eccd744baf7eb8221843667a172
Rucha Shashikant Tare <76004350+RuchaTare@users.noreply.github.com>
2022-11-07 13:25:17 +0000

    Time: 1077 ms (57.75%), Space: 0B (100.00%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+
+#we need unique email ids from email column person table
+# smallest id must stay if duplicate
+#note : Delete works with table name without column name
+
+DELETE p1 from Person p1
+JOIN Person p2
+ON p1.email = p2.email
+WHERE p1.id > p2.id

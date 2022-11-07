489ca5c1e37341b5defcb8a36dc36229222da905
Rucha Shashikant Tare <76004350+RuchaTare@users.noreply.github.com>
2022-11-07 13:27:21 +0000

    Time: 611 ms (94.32%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -5,7 +5,10 @@
 # smallest id must stay if duplicate
 #note : Delete works with table name without column name
 
-DELETE p1 from Person p1
-JOIN Person p2
-ON p1.email = p2.email
-WHERE p1.id > p2.id
+# DELETE p1 from Person p1
+# JOIN Person p2
+# ON p1.email = p2.email
+# WHERE p1.id > p2.id
+
+DELETE p from Person p where id NOT IN (SELECT * FROM(
+    SELECT MIN(Id) FROM Person GROUP BY Email) as p);

036a58163c28c66d662f9b721e0a4566adfa9eee
Rucha Shashikant Tare <76004350+RuchaTare@users.noreply.github.com>
2022-11-08 09:59:09 +0000

    Time: 975 ms (66.27%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -3,15 +3,15 @@
 # Left outer join on empid
 # find bonus < 1000
 
-# SELECT e.name, b.bonus 
-# FROM Employee e
-# LEFT JOIN Bonus b
-# ON e.empId = b.empId
-# WHERE ifnull(b.bonus, 0) < 1000
-
 SELECT e.name, b.bonus 
 FROM Employee e
 LEFT JOIN Bonus b
 ON e.empId = b.empId
-WHERE b.bonus < 1000 
-or b.bonus IS NULL
+WHERE ifnull(b.bonus, 0) < 1000
+
+# SELECT e.name, b.bonus 
+# FROM Employee e
+# LEFT JOIN Bonus b
+# ON e.empId = b.empId
+# WHERE b.bonus < 1000 
+# or b.bonus IS NULL
